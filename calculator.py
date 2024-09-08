6131a6ba29b0bd7ff078842bdc8ecab19df336ea
Travis M <>
2024-09-07 18:36:01 -0700

    Add solution to PY101 Lesson 2 Problem 10 "Refactoring Calculator"

--- calculator.py
+++ calculator.py
@@ -5,29 +5,61 @@
 # Print the result to the terminal.
 
 
-print("Welcome to Calculator")
+def prompt(message):
+    print(f"==> {message}")
 
-num1 = int(input("What's the first number? "))
-num2 = int(input("What's the second number? "))
 
-print(f"You entered {num1} and {num2}.")
+def invalid_number(num_str):
+    try:
+        float(num_str)
+    except ValueError:
+        return True
+    return False
 
-operation = input(
+
+prompt("Welcome to Calculator!")
+
+prompt("What's the first number?")
+num1 = input()
+
+while invalid_number(num1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    num1 = input()
+
+prompt("What's the second number?")
+num2 = input()
+
+while invalid_number(num2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    num2 = input()
+
+prompt(f"You entered {num1} and {num2}.")
+
+prompt(
     "What operation do you want to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide: "
 )
+operation = input()
 
-if operation == "1":
-    answer = num1 + num2
-elif operation == "2":
-    answer = num1 - num2
-elif operation == "3":
-    answer = num1 * num2
-elif operation == "4":
-    if num2 == 0:
-        answer = "You cannot divide by zero."
-    else:
-        answer = num1 / num2
+while operation not in ["1", "2", "3", "4"]:
+    prompt("You must choose 1, 2, 3, or 4")
+    operation = input()
+
+answer = None
+
+match operation:
+    case "1":
+        answer = float(num1) + float(num2)
+    case "2":
+        answer = float(num1) - float(num2)
+    case "3":
+        answer = float(num1) * float(num2)
+    case "4":
+        answer = float(num1) / float(num2)
+    case _:
+        answer = "Invalid selection"
+
+
+if answer is not None:
+    prompt(f"The result is: {answer}")
 else:
-    answer = "Input error"
-
-print(f"The result is: {answer}")
+    prompt("Invalid operation selected.")
