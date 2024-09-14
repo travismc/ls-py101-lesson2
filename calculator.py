73a5ed13ec20ab2ca2514c10dd62b90ae2b4bae2
Travis M <>
2024-09-13 17:10:45 -0700

    Added calculator_messages.json integration.

--- calculator.py
+++ calculator.py
@@ -4,6 +4,8 @@
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
+import json
+
 
 def prompt(message):
     print(f"==> {message}")
@@ -17,36 +19,42 @@ def invalid_number(num_str):
     return False
 
 
+def messages(message, lang="en"):
+    return MESSAGES[lang][message]
+
+
+with open("calculator_messages.json", "r") as file:
+    MESSAGES = json.load(file)
+
+
 exit = False
 answer = "n"
 
-while exit is False:
-    prompt("Welcome to Calculator!")
 
-    prompt("What's the first number?")
+while exit is False:
+    prompt(messages("welcome"))
+
+    prompt(messages("number_prompt_1"))
     num1 = input()
 
     while invalid_number(num1):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt(messages("invalid_number"))
         num1 = input()
 
-    prompt("What's the second number?")
+    prompt(messages("number_prompt_2"))
     num2 = input()
 
     while invalid_number(num2):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt(messages("invalid_number"))
         num2 = input()
 
     prompt(f"You entered {num1} and {num2}.")
 
-    prompt(
-        """What operation do you want to perform?
-    1) Add 2 Subtract 3) Multiply 4) Divide: """
-    )
+    prompt(messages("operation_prompt"))
     operation = input()
 
     while operation not in ["1", "2", "3", "4"]:
-        prompt("You must choose 1, 2, 3, or 4")
+        prompt(messages("invalid_operation"))
         operation = input()
 
     # output = None
